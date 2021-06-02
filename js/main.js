c7afff19a34a23fdf8790802d9b7ec13a56c30c1
ViktorEgai <egai.viktor94@gmail.com>
2021-06-03 00:27:23 +0300

    Сверстал полностью секцию articles

--- js/main.js
+++ js/main.js
@@ -17,4 +17,12 @@ $(document).ready(function () {
     $(".header-navbar")
     .toggleClass("header-navbar--visible");
   });
+
+  // плавный скролл якорных ссылок
+  $("#navbar").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
 });
